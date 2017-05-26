3fbb6f1a1b1156bee73ccc650e446737ba5df0b1
Swall0w <technext.jpn@gmail.com>
2017-05-26 20:18:56 +0900

    modified.

--- index.py
+++ index.py
@@ -4,4 +4,9 @@ from bottle import route, run, template
 def index(name):
     return template('<b>hello {{name}}</b>!',name=name)
 
-run(host='localhost', port=8080)
+@route('/')
+def index():
+    return '<h1>WELCOME TO NEW WORLD</h1>'
+
+
+run(host='localhost', port=8080,debug=True,reloader=True)
